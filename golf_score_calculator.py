77e0ad8f02ae90a8842d7a85acd169ae118186b4
Shane Bell <sbell@nait.ca>
2026-02-23 11:44:39 -0700

    Feb 23

---
+++ golf_score_calculator.py
@@ -0,0 +1,39 @@
+print("Golf Score Calculator")
+
+sum = 0
+scores = []
+top_5_sum = 0
+
+while True:
+    #get scores from the user
+    score = input("Enter another score: (q to quit and calculate): ")
+    #if q then quit the loop
+    if score == "q":
+        break
+    #Add score to a sum
+    sum += int(score)        
+    scores.append(int(score))
+
+#calculate average
+average = sum/len(scores)    
+#display average
+print(f"Your average golf score is {average}")
+
+#is there 5 scores to calculate handicap?
+if len(scores) < 5:
+    print("Your handicap is: Need at least 5 scores to calculate a handicap")
+else:    
+#Sort the list ascending
+    scores.sort()
+
+    #Calculate the sum of the first 5 scores in the list and put in a variable
+    for score in scores[0:5]:
+        top_5_sum += score
+
+    #calculate the average of the first 5 scores and subtract 72
+    handicap = top_5_sum/5 - 72
+
+    #print handicap
+    print(f"Your handicap is: {handicap:.2f}")
+
+
---
+++ sort_exercise.py
@@ -0,0 +1,33 @@
+#create a list of 100 random numbers between 1 and 1000
+    #no duplicates!
+
+
+#SORT in ascending order using loops/variables. 
+
+1,5,2,6,3
+1,5,2,3,6 #swap the 3 and 6
+1,5,2,3,6 #no change because 2 and 3 in correct order
+1,2,5,3,6 #swap 2 and 5
+1,2,5,3,6 # no change because 1 and 2 in correct order
+#go back to end of list
+1,2,5,3,6#no change because 3 and 6 in correct order
+1,2,3,5,6#swap 5 and 3
+1,2,3,5,6#no change
+1,2,3,5,6#no change
+
+#keep swapping unil you are able to loop through without any swaps
+#The higher number eventualy filters its way closer to the end of the list
+1,2,3,5,6
+
+right = 3
+left = right - 1
+
+
+1,5,2,6,3
+
+if numbers[left] > numbers[right]:
+    #swap
+
+
+
+
